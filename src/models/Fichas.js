81f6b26b8e4d6d46b62788ff182cf883af7ed783
Your Name <you@example.com>
2024-09-12 21:33:36 -0500

    bitacorass

--- src/models/Fichas.js
+++ src/models/Fichas.js
@@ -1,9 +1,10 @@
 // const mongoose = require('mongoose')
 import mongoose from "mongoose"
+
 const fichaSchema = new mongoose.Schema({
     Codigo: {type:String, require:true, unique:true,},
     Nombre:{type:String, require:true, max:50},
     Estado: {type:Number, require:true, default:1}
 })
 
-export default  mongoose.model("Ficha", fichaSchema )
+export default mongoose.model("Ficha", fichaSchema )
