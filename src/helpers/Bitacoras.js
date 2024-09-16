417bc1544ca472d4e5916281e2dd4a3ddf3440a1
Tatiana <tatianamantilla547@gmail.com>
2024-09-16 11:58:51 -0500

    cambios en rutas de bitacoras5

--- src/helpers/Bitacoras.js
+++ src/helpers/Bitacoras.js
@@ -4,14 +4,15 @@ import Aprendices from "../models/Aprendices.js"
 
 const bitacoraHelper = {
     existeDocAprendiz: async (Documento)=>{
-    const exists = await Aprendices.find({Documento})
+    const exists = await Aprendices.findOne({Documento})
+
     if(!exists){
         throw new Error("EL Documento del aprendiz no existe")
     }
 },
 
 unicoDocAprendiz: async (Documento)=>{
-    const unico = await Aprendices.find({Documento})
+    const unico = await Aprendices.findOne({Documento})
     if (unico.length > 0) {
         throw new Error("El Documento del aprendiz ya existe");
     }
