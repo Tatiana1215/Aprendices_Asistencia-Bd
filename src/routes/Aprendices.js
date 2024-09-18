4101e0968ed0b0d44e44410ff06548e7cc15c06d
Tatiana <tatianamantilla547@gmail.com>
2024-09-18 15:50:54 -0500

    parte de fronet

--- src/routes/Aprendices.js
+++ src/routes/Aprendices.js
@@ -46,7 +46,7 @@ routers.get("/listarPorFicha/:Id_Ficha", [
 // -------------------------------------------------------------------------------------------------------------------------
 routers.post("/Insertar",upload.single('firma'), [
     uploadFirma,
-    validarJWT,
+    // validarJWT,
     check('Nombre', 'El campo Nombre es obligatorio').notEmpty(),
     check('Documento', 'El campo documento es obligatorio').notEmpty(),
     check('Telefono', 'El campo telefono es obligatorio').notEmpty(),
