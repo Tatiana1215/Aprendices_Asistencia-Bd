014275bcd18a8aa54d0dc2ee14f7391c435278d6
Tatiana <tatianamantilla547@gmail.com>
2024-09-19 16:12:11 -0500

    Nuevos Cambios del Bakend1

--- src/models/Aprendices.js
+++ src/models/Aprendices.js
@@ -2,13 +2,22 @@
 import mongoose from 'mongoose'
 
 const aprendicesSchema = new mongoose.Schema({
-    Documento: { type: String, require: true, unique: true, min: 10 },
-    Nombre: { type: String, require: true, max: 20 },
-    Telefono: { type: String, require: true, unique: true, min: 10 },
-    Email: { type: String, require: true, unique: true },
-    Estado: { type: Number, require: true, default: 1 },
+    // Documento: { type: String, require: true, unique: true, min: 10 },
+    // Nombre: { type: String, require: true, max: 20 },
+    // Telefono: { type: String, require: true, unique: true, min: 10 },
+    // Email: { type: String, require: true, unique: true },
+    // Estado: { type: Number, require: true, default: 1 },
+    // Firma: { type: String, // Aquí almacenamos la ruta del archivo de la firma
+    //          required: false // Este campo es opcional
+    // },
+    // Id_Ficha: { type: mongoose.Schema.Types.ObjectId, ref: 'Ficha' }
+    Documento: { type: String },
+    Nombre: { type: String,  },
+    Telefono: { type: String,  },
+    Email: { type: String, },
+    Estado: { type: Number, },
     Firma: { type: String, // Aquí almacenamos la ruta del archivo de la firma
-             required: false // Este campo es opcional
+            // Este campo es opcional
     },
     Id_Ficha: { type: mongoose.Schema.Types.ObjectId, ref: 'Ficha' }
 })
