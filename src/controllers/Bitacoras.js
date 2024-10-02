01b9e332608b2188cec8caf22653da1be4747e9b
Tatiana <tatianamantilla547@gmail.com>
2024-10-02 18:56:44 -0500

    filtro

--- src/controllers/Bitacoras.js
+++ src/controllers/Bitacoras.js
@@ -109,8 +109,8 @@ const httpBitacoras = {
             // Buscar bitácoras para los aprendices entre las fechas especificadas
             const bitacoras = await Bitacoras.find({
                 createdAt: { $gte: fechaInicial, $lte: fechaFinal },
-                Id_Aprendiz: { $in: aprendices.map(a => a._id) },
-                Estado: 'Asistio'
+                // Id_Aprendiz: { $in: aprendices.map(a => a._id) },
+                // Estado: 'Asistio'
             }).populate('Id_Aprendiz', 'Nombre Documento Email Telefono');  // Popula los datos del aprendiz
 
             // Si no hay bitácoras, responde con un mensaje adecuado
