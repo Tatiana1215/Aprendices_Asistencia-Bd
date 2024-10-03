79c970490a7ad8922529d8565177e4904e87332c
Tatiana <tatianamantilla547@gmail.com>
2024-10-03 18:51:49 -0500

    usuario

--- src/routes/Usuarios.js
+++ src/routes/Usuarios.js
@@ -1,3 +1,106 @@
+// import { check } from "express-validator";
+// import {validarCampos} from "../middlewares/validar-campos.js"
+// import {validarJWT} from "../middlewares/validarJWT.js"
+// import {httpUsarios} from "../controllers/Usuarios.js"
+// import { usuarioHelper } from "../helpers/Usuarios.js";
+// import { Router } from "express";
+
+// const routers = Router()
+
+// // const nodemailer = require('nodemailer');
+
+// // -----------------------------------------------------------------------------------------------------------------------------------
+// routers.get("/listarTodos",[
+//     // validarCampos,
+//     // validarJWT
+// ],httpUsarios.getUsuarios)
+
+// // routers.get("/listarPorUsuario",[
+// //     validarCampos,
+// // ])
+
+
+// // --------------------------------------------------------------------------------------------------------------------------------------
+// routers.post('/login',[
+//     check('Email','El campo email es obligatorio').notEmpty(),
+//     check('Password','El campo de contraseña es obligatorio').notEmpty(),
+//     validarCampos
+// ], httpUsarios.postLogin)
+
+// // Ruta para solicitar recuperación de contraseña-----------------------------------------------------------------------------------------
+// routers.post('/solicitar-recuperacion', [
+//     check('Email','El campo email es obligatorio').notEmpty(),
+//     validarCampos
+// ],httpUsarios.solicitarRecuperacionContrasena);
+// routers.post('/Verificacion',[
+//     check('Email','El campo email es obligatorio').notEmpty(),
+//     check('Codigo' , 'El campo Codigo es obligatorio').notEmpty(),
+//     validarCampos 
+// ],httpUsarios.postVerificarCodigo );
+
+// // Ruta para restablecer la contraseña-----------------------------------------------------------------------------------------------------
+// routers.post('/reset/:id', [
+//     check('oldpassword', 'El campo de la contraseña actual es obligatorio').notEmpty(),
+//     check('Password', 'El campo del password es obligatorio').notEmpty(),
+//     check('Password','La contraseña debe tener minimo 10 caracteres y maximo 15').isLength({min:10, max:15}),
+//     validarCampos
+// ],httpUsarios.restablecerContrasena);
+
+
+// // -------------------------------------------------------------------------------------------------------------------------------------
+// routers.post("/insertar",[
+//     // validarJWT,
+// check('Nombre','El campo Nombre es obligatorio').notEmpty(),
+// check('Email','El campo Email es obligatorio').notEmpty().isEmail,
+// check('Password','El campo password es obligatorio').notEmpty(),
+// check('Email').custom(usuarioHelper.existsEmail),
+// // check('Password').custom(usuarioHelper.existePassword),
+// check('Nombre','El Nombre debe tener maximo 30 caracteres').isLength({max:30}),
+// check('Password','La contraseña debe tener minimo 10 caracteres y maximo 15').isLength({min:10, max:15}),
+// validarCampos,
+// ],httpUsarios.postUsuario)
+
+// // // -----------------------------------------------------------------------------------------------------------------------------------------
+// routers.put("/Actualizar/:id",[
+//     // validarJWT,
+//     check('id','El id no es valido').isMongoId(),
+//     // check('Email').custom(usuarioHelper.existsEmail),
+//     // check('Email','El email no es correcto').isEmail(),
+//     // check('Password').custom(usuarioHelper.existePassword),
+//     // check('Password','La contraseña debe tener minimo 10 caracteres y maximo 15').isLength({min:10, max:15}),
+//     check('Nombre','El Nombre debe tener maximo 30 caracteres').isLength({max:30}),
+//     check('Email').custom(async (Email, { req }) => {
+//         await usuarioHelper.esEmailid(Email, req.params.id);
+//     }),
+//     validarCampos
+// ],httpUsarios.putUsuarioActualizar)
+
+// // --------------------------------------------------------------------------------------------------------------------------------------------
+// routers.put("/Activar/:id",[
+//     validarJWT,
+//     check('id','El no es valido').isMongoId(),
+//     validarCampos,
+//     // validarJWT
+// ],httpUsarios.putUsuarioActivar)
+
+// // -------------------------------------------------------------------------------------------------------------------------------------------
+// routers.put("/Desactivar/:id",[
+//     validarJWT,
+//     check('id','El id no es valido').isMongoId(),
+//     validarCampos,
+//     // validarJWT
+// ],httpUsarios.putUsuarioDesactivar)
+
+// // ------------------------------------------------------------------------------------------------------------------------------------------
+// routers.delete("/Eliminar/:id",[
+//     check('id','El id no es valido').isMongoId(),
+//     validarCampos,
+//     validarJWT
+// ], httpUsarios.deleteUsario)
+
+// export default routers
+
+
 import { check } from "express-validator";
 import {validarCampos} from "../middlewares/validar-campos.js"
 import {validarJWT} from "../middlewares/validarJWT.js"
@@ -51,7 +154,7 @@ routers.post('/reset/:id', [
 routers.post("/insertar",[
     // validarJWT,
 check('Nombre','El campo Nombre es obligatorio').notEmpty(),
-check('Email','El campo Email es obligatorio').notEmpty().isEmail,
+check('Email','El campo Email es obligatorio').notEmpty(),
 check('Password','El campo password es obligatorio').notEmpty(),
 check('Email').custom(usuarioHelper.existsEmail),
 // check('Password').custom(usuarioHelper.existePassword),
@@ -77,7 +180,6 @@ routers.put("/Actualizar/:id",[
 
 // --------------------------------------------------------------------------------------------------------------------------------------------
 routers.put("/Activar/:id",[
-    validarJWT,
     check('id','El no es valido').isMongoId(),
     validarCampos,
     // validarJWT
@@ -85,7 +187,6 @@ routers.put("/Activar/:id",[
 
 // -------------------------------------------------------------------------------------------------------------------------------------------
 routers.put("/Desactivar/:id",[
-    validarJWT,
     check('id','El id no es valido').isMongoId(),
     validarCampos,
     // validarJWT
